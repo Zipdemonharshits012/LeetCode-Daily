fa8a0deac5b7fa0ddb07d38090a81fe85a635bc2
Zipdemonharshits012 <Zipdemonharshits012@gmail.com>
2021-07-17 13:00:27 +0530

    solved merged Intervals

---
+++ 56_Merge Intervals.cpp
@@ -0,0 +1,21 @@
+//Greedy Algo Sorting Imp Question 
+//Link - https://leetcode.com/problems/merge-intervals/
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& fre) {
+        vector<vector<int>> v;
+        sort(fre.begin(),fre.end());
+        v.push_back(fre[0]);
+        int j=0;
+        for(int i=1;i<fre.size();i++){
+            if(v[j][1]>=fre[i][0]){
+                v[j][1]=max(v[j][1],fre[i][1]);
+            }
+            else{
+                v.push_back(fre[i]);
+                j++;
+            }
+        }
+        return v;
+    }
+};
