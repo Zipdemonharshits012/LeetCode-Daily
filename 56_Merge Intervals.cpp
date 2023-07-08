6d9f5bc3553f6c1f82a254d7ecc9792bb47a7e4c
harshitlarl <64593254+harshitlarl@users.noreply.github.com>
2023-07-08 06:31:05 +0530

    Update 56_Merge Intervals.cpp

--- 56_Merge Intervals.cpp
+++ 56_Merge Intervals.cpp
@@ -25,3 +25,19 @@ QUtJQVVNTkE1SkZBVTVSRFhIRUc=
 AKIAUMNA5JFAXO65BXNJ
 WCCgBZuC19LCJOFlkBgKFoeqYoWzR+7SzfHD04M3
 V0NDZ0JadUMxOUxDSk9GbGtCZ0tGb2VxWW9XelIrN1N6ZkhEMDRNMw0K
+
+
+
+{
+  "type": "service_account",
+  "project_id": "my-project",
+  "private_key_id": "abcdef1234567890",
+  "private_key": "-----BEGIN PRIVATE KEY-----\n<Private Key Content>\n-----END PRIVATE KEY-----\n",
+  "client_email": "service-account@my-project.iam.gserviceaccount.com",
+  "client_id": "123456789012345678901",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/service-account%40my-project.iam.gserviceaccount.com"
+}
+
